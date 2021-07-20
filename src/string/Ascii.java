7c056cc2715c2917b541a5df86679c384bf5306e
nageom <nageom1123@naver.com>
2021-07-20 22:37:39 +0900

    11654

---
+++ src/string/Ascii.java
@@ -0,0 +1,19 @@
+package string;
+
+import java.util.Scanner;
+
+public class Ascii {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("´Ü¾î");
+				 
+		int ascii = scan.next().charAt(0);
+		
+		System.out.println(ascii);
+		
+		
+	}
+	
+}
