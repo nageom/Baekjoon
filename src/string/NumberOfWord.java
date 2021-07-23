0c8ab8b688d10339301bb1a493df82085b915bc3
nageom <nageom1123@naver.com>
2021-07-23 23:27:21 +0900

    1152번 수정

--- src/string/NumberOfWord.java
+++ src/string/NumberOfWord.java
@@ -12,10 +12,12 @@ public class NumberOfWord {
 		
 		String[] arr = word.split(" ");
 		
-		for(String a : arr) {
-			System.out.println(a);
+		for (int i=0; i<arr.length; i++) {
 			
+			System.out.println(arr[i]);
 		}
+		
+		System.out.println(arr.length);
 		
 		
 		
