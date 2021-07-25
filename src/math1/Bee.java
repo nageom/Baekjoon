97606ed96f9de9b50a73ce196db95635869de851
nageom <nageom1123@naver.com>
2021-07-25 23:44:50 +0900

    2292번 백준

---
+++ src/math1/Bee.java
@@ -0,0 +1,24 @@
+package math1;
+
+import java.util.Scanner;
+
+public class Bee {
+
+	public static void main(String[] args) {
+		
+		//커지는 육각형
+		// 6의 배수로 개수가 커진다 
+	
+		Scanner scan = new Scanner(System.in);
+		System.out.println("입력");
+		int a = scan.nextInt();
+		
+		int tmp = a/6+1;
+		
+		System.out.println("출력"+tmp);
+		// 악 틀렸다 
+		
+		
+		
+	}
+}
