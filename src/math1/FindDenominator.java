19927b18a5aef4400141541952400cb8bcc4672e
nageom <nageom1123@naver.com>
2021-08-08 23:32:07 +0900

    update

--- src/math1/FindDenominator.java
+++ src/math1/FindDenominator.java
@@ -1,10 +1,43 @@
 package math1;
 
+import java.util.Scanner;
+
 public class FindDenominator {
+	
 	public static void main(String[] args) {
+		// n =10;  (4,1)  4/1
+		/* n = 12; (4,2)  4/2
+		 * n = 3 ; (2,1) 
+		 *  
+		 * */
+		// 알아야할것 
+		//1. 대각선 한 줄의 각 분수는 분자와 분모 값을 더한 값이 모두 같다. 
+		//2. 홀수는 아래에서 위로, 짝수는 위에서 아래로 카운트 
+		
+		//사용 변수
+		// 입력 값 n 
+		// 해당 범위의 대각선 개수 (count)
+		// 해당 대각선 직전 대각선 까지의 간의 누적 합(sum) 
+		
+		// n - sum = 현재 대각선에서 몇번째 분수인지 알 수 있다. 
+		
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		
+		int cCount = 1; // 해당 범위 대각선 개수 
+		int cSum = 0;
+		
+		while(true) {
+			
+			
+			
+		}
 		
 		
 		
-	}
+		
+		
+		
+	}   
 
 }
