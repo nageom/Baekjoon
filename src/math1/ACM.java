bfa573925d3515f65ca0cda92a4d165e7381dd77
nageom <nageom1123@naver.com>
2021-08-09 23:21:53 +0900

    update

--- src/math1/ACM.java
+++ src/math1/ACM.java
@@ -1,5 +1,25 @@
 package math1;
 
-public class ACM {
+import java.util.Scanner;
 
+public class ACM {
+	
+	public static void main(String[] args) {
+		/*
+		 * 입력값 T : 테스트케이스 개수 
+		 * 높이 H, 넓이 W, 몇번째 손님인지 N 
+		 * 
+		 * 
+		 * */
+		
+		Scanner	scan = new Scanner(System.in);
+		// 순서는 101호부터 201호 쭉쭉 올라가서 높이H 를 찍고
+		// 다시 102호부터 202호 
+		// 즉 몇 번째 인지 받는 정수를 높이 H로 나누고 몫은 호수의 뒷번호 XX가 되고
+		// 나머지는 층수 YY가 될 것 같은데 ? 
+		
+		
+		
+		
+	}
 }
